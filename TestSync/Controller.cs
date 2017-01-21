ae90a8f65604797fa4ddd2903dc254270ba3eb8d
Oleksandr Krokhmal <alexkrokhmall@gmail.com>
2017-01-21 15:29:28 +0200

    A bit advanced example, now developed by me. Can sync several sources in TWO WAYS ONLY. Issue with order of syncing

---
+++ TestSync/Controller.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+
+namespace TestSync
+{
+    public class SyncronizationController
+    {
+        private FileSyncScopeFilter filter = new FileSyncScopeFilter();
+        private string[] rootPathsArray = new string[] { "D:\\1", "D:\\2", "D:\\3"};
+
+        private bool isSyncInProgress = false;
+
+
+
+        //Converts string from text box in UI to string array of dedicated filters, by ';' separator
+        public string[] parseSyncFiltersFromView(string syncFiltersFromView)
+        {
+            string[] filtersArray = null;
+
+            //Business logic
+            filtersArray = new string[1] { "*.rar"};
+
+            return filtersArray;
+        }
+
+        //Re-Creates includeFiles and excludeFiles filters according to parsed from UI values
+        public void updateSyncFilters(string[] excludeFiltersArray, string[] includeFilterArray)
+        {
+            if (excludeFiltersArray != null && excludeFiltersArray.Length > 0)
+            {
+                filter.FileNameExcludes.Clear();
+                for (int i = 0; i < excludeFiltersArray.Length; i++)
+                {
+                    filter.FileNameExcludes.Add(excludeFiltersArray[i]);
+                }
+            }
+            if (includeFilterArray != null && includeFilterArray.Length > 0)
+            {
+                filter.FileNameIncludes.Clear();
+                for (int i = 0; i < includeFilterArray.Length; i++)
+                {
+                    filter.FileNameIncludes.Add(includeFilterArray[i]);
+                }
+            }
+        }
+
+        //Runs sycronization of folders provided
+        public void RunSyncronization()
+        {
+            if (!isSyncInProgress)
+            {
+                isSyncInProgress = true;
+
+                FoldersSyncronizator folderSyncronizator = new FoldersSyncronizator();
+
+                folderSyncronizator.Filter = filter;
+                folderSyncronizator.RootPathsArray = rootPathsArray;
+
+                folderSyncronizator.DetectChangesInAllRootFolders();
+                folderSyncronizator.syncAllFolders();
+
+                isSyncInProgress = false;
+            }
+            else
+            {
+                Console.WriteLine("The sync is still in progress");
+            }            
+        }
+
+
+
+    }
+}
---
+++ TestSync/FoldersSyncronizator.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+
+namespace TestSync
+{
+    public class FoldersSyncronizator
+    {
+        public string[] RootPathsArray { set; get; }
+        public FileSyncScopeFilter Filter { set; get; }
+
+        public FoldersSyncronizator()
+        { }
+
+        
+        public void DetectChangesInAllRootFolders()
+        {
+            FileSyncProvider provider = null;
+
+            try
+            {
+                for (int i = 0; i < RootPathsArray.Length; i++)
+                {
+                    provider = new FileSyncProvider(RootPathsArray[i], Filter, FileSyncOptions.None);
+                    provider.DetectChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception from 'DetectChangesInAllRootFolders': {0}", e.ToString());
+                //throw e;
+            }
+            finally
+            {
+                if (provider != null)
+                {
+                    provider.Dispose();
+                }
+            }
+
+        }
+
+        public void DetectChangesInAllRootFolders(string[] rootPathsArray, FileSyncScopeFilter filter)
+        {
+            FileSyncProvider provider = null;
+
+            try
+            {
+                for (int i = 0; i < RootPathsArray.Length; i++)
+                {
+                    provider = new FileSyncProvider(rootPathsArray[i], filter, FileSyncOptions.None);
+                    provider.DetectChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception from 'DetectChangesInAllRootFolders': {0}", e.ToString());
+                //throw e;
+            }
+            finally
+            {
+                if (provider != null)
+                {
+                    provider.Dispose();
+                }
+            }
+
+        }
+        
+        public void syncAllFolders()
+        {
+            for (int i = 1; i < RootPathsArray.Length; i++)
+            {
+                syncTwoFoldersOneWayDownload(RootPathsArray[0], RootPathsArray[i]);
+            }
+            for (int i = 1; i < RootPathsArray.Length; i++)
+            {
+                syncTwoFoldersOneWayUpload(RootPathsArray[0], RootPathsArray[i]);
+            }
+        }
+
+        public void syncAllFolders(string[] rootPathsArray, FileSyncScopeFilter filter)
+        {
+            for (int i = 1; i < rootPathsArray.Length; i++)
+            {
+                syncTwoFoldersOneWayDownload(RootPathsArray[0], RootPathsArray[i],filter);
+            }
+            for (int i = 1; i < rootPathsArray.Length; i++)
+            {
+                syncTwoFoldersOneWayUpload(RootPathsArray[0], RootPathsArray[i], filter);
+            }
+        }
+
+        private void syncTwoFoldersOneWayUpload(string sourceRootPath, string destinationRootPath)
+        {
+            FileSyncProvider sourceProvider = null;
+            FileSyncProvider destinationProvider = null;
+
+            try
+            {
+                sourceProvider = new FileSyncProvider(sourceRootPath, Filter, FileSyncOptions.None);
+                destinationProvider = new FileSyncProvider(destinationRootPath, Filter, FileSyncOptions.None);
+
+                SyncOrchestrator orchrstrator = new SyncOrchestrator();
+                orchrstrator.Direction = SyncDirectionOrder.Upload;
+                orchrstrator.LocalProvider = sourceProvider;
+                orchrstrator.RemoteProvider = destinationProvider;
+
+                orchrstrator.Synchronize();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sourceProvider != null)
+                {
+                    sourceProvider.Dispose();
+                }
+                if (destinationProvider != null)
+                {
+                    destinationProvider.Dispose();
+                }
+            }
+        }
+
+        private void syncTwoFoldersOneWayUpload(string sourceRootPath, string destinationRootPath, FileSyncScopeFilter filter)
+        {
+            FileSyncProvider sourceProvider = null;
+            FileSyncProvider destinationProvider = null;
+
+            try
+            {
+                sourceProvider = new FileSyncProvider(sourceRootPath, filter, FileSyncOptions.None);
+                destinationProvider = new FileSyncProvider(destinationRootPath, filter, FileSyncOptions.None);
+
+                SyncOrchestrator orchrstrator = new SyncOrchestrator();
+                orchrstrator.Direction = SyncDirectionOrder.Upload;
+                orchrstrator.LocalProvider = sourceProvider;
+                orchrstrator.RemoteProvider = destinationProvider;
+
+                orchrstrator.Synchronize();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sourceProvider != null)
+                {
+                    sourceProvider.Dispose();
+                }
+                if (destinationProvider != null)
+                {
+                    destinationProvider.Dispose();
+                }
+            }
+        }
+
+        private void syncTwoFoldersOneWayDownload(string sourceRootPath, string destinationRootPath)
+        {
+            FileSyncProvider sourceProvider = null;
+            FileSyncProvider destinationProvider = null;
+
+            try
+            {
+                sourceProvider = new FileSyncProvider(sourceRootPath, Filter, FileSyncOptions.None);
+                destinationProvider = new FileSyncProvider(destinationRootPath, Filter, FileSyncOptions.None);
+
+                SyncOrchestrator orchrstrator = new SyncOrchestrator();
+                orchrstrator.Direction = SyncDirectionOrder.Download;
+                orchrstrator.LocalProvider = sourceProvider;
+                orchrstrator.RemoteProvider = destinationProvider;
+
+                orchrstrator.Synchronize();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sourceProvider != null)
+                {
+                    sourceProvider.Dispose();
+                }
+                if (destinationProvider != null)
+                {
+                    destinationProvider.Dispose();
+                }
+            }
+        }
+
+        private void syncTwoFoldersOneWayDownload(string sourceRootPath, string destinationRootPath, FileSyncScopeFilter filter)
+        {
+            FileSyncProvider sourceProvider = null;
+            FileSyncProvider destinationProvider = null;
+
+            try
+            {
+                sourceProvider = new FileSyncProvider(sourceRootPath, filter, FileSyncOptions.None);
+                destinationProvider = new FileSyncProvider(destinationRootPath, filter, FileSyncOptions.None);
+
+                SyncOrchestrator orchrstrator = new SyncOrchestrator();
+                orchrstrator.Direction = SyncDirectionOrder.Download;
+                orchrstrator.LocalProvider = sourceProvider;
+                orchrstrator.RemoteProvider = destinationProvider;
+
+                orchrstrator.Synchronize();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sourceProvider != null)
+                {
+                    sourceProvider.Dispose();
+                }
+                if (destinationProvider != null)
+                {
+                    destinationProvider.Dispose();
+                }
+            }
+        }
+
+
+
+    }
+}
